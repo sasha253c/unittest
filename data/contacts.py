c7bbd651c835345279b17a244fffb57d985e6739
sasha253c <kochynsasha253@gmail.com>
2016-07-19 18:57:46 +0300

    Added contact.json

---
+++ data/contacts.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname='firstname1', lastname='lastname1'),
+    Contact(firstname='firstname2', lastname='lastname2'),
+]
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,9 @@
 #! usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import pytest
 
-from data.add_group import testdata
-#from data.add_group import constant as testdata
-
-
-
-@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contact_list = app.contact.get_contact_list()
     app.contact.add(contact)
     assert len(old_contact_list) + 1 == app.contact.count()
